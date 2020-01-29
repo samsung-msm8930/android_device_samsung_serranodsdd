d9816066099aa0f21341e0c5bfc17bc9ad98f883
ripee <ripee.dev@gmail.com>
2020-01-29 02:15:39 +0100

    serranodsdd: AOSP-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_serranodsdd.mk
+    $(LOCAL_DIR)/aosp_serranodsdd.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo lineage_serranodsdd-userdebug
+add_lunch_combo aosp_serranodsdd-userdebug
